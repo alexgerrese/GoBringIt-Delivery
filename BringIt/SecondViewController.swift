6f70592cadc1f900f467475560a935574bcfc013
Alexander G <alexandergerrese@gmail.com>
2016-05-13 17:19:48 -0500

    Initial Commit

---
+++ BringIt/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BringIt
+//
+//  Created by Alexander's MacBook on 5/13/16.
+//  Copyright © 2016 Campus Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
