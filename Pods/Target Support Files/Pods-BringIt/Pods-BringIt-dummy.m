e9cb2f833fb499f2aacd49bb26bb2df10e2f834d
Alexander G <alexandergerrese@gmail.com>
2016-05-13 17:22:16 -0500

    Initial Commit - with CocoaPods

---
+++ Pods/Target Support Files/Pods-BringIt/Pods-BringIt-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BringIt : NSObject
+@end
+@implementation PodsDummy_Pods_BringIt
+@end
---
+++ Pods/Target Support Files/Pods-BringItUITests/Pods-BringItUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BringItUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BringItUITests
+@end
