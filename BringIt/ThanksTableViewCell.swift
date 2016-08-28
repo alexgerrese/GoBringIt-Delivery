76de02ffde95f94ccefbcf8faa86044bd1042e8e
Alexander G <alexandergerrese@gmail.com>
2016-08-28 01:58:37 -0400

    - Added pods

---
+++ BringIt/ThanksTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ThanksTableViewCell.swift
+//  BringIt
+//
+//  Created by Alexander's MacBook on 8/17/16.
+//  Copyright © 2016 Campus Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ThanksTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var gotItButton: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        gotItButton.layer.cornerRadius = 17
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
